1052a517cda7005229795cc3b2bbfef69eaebd18
cleverson <cleverson.desenvolvedor@gmail.com>
2023-01-02 18:43:35 -0300

    first commit

---
+++ Praticar para aperfeiçoar/praticar.js
@@ -0,0 +1,9 @@
+/*
+  Praticar para aperfeiçoar
+
+    Descrição
+
+      Esse é o módulo que vai separar os meninos dos homens, as meninas das mulheres, é esse que vai mostrar para você se realmente aprendeu o que havia no curso, mas não se preocupe, pois o maykão vai resolver os desafios depois que forem passados, mas não veja a resolução antes de tentar sozinho.
+
+      Vamos fortalecer e aplicar os conhecimentos, além de aprender truques e dicas para resolver os desafios e avançar para o próximo nível.
+*/
